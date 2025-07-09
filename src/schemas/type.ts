701255bbf67378cbde730eecc4bf52ff0aa46706
Tirth P Shah <cptirthp.shah@gmail.com>
2025-07-09 20:30:46 +0530

    Checked for production: OK

--- src/schemas/type.ts
+++ src/schemas/type.ts
@@ -1,9 +1,14 @@
 export interface FieldSchema {
+    /**
+     * If true, allows multiple values (e.g., multiple files for file input).
+     */
+    multiple?: boolean;
     name: string;
     label: string;
     type: 'text' | 'number' | 'date' | 'select' | 'radio' | 'checkbox' | 'textarea' | 'file' | 'datalist' | 'email' | 'phone';
     placeholder?: string;
     options?: string[];
     required?: boolean;
+    group?: string;
     visible?: (values: any) => boolean;
 }
