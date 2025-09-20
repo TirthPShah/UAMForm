88eccec37297cdcb7de808209eec46ab24cf27bb
Tirth P Shah <cptirthp.shah@gmail.com>
2025-09-20 14:10:56 +0530

    Added redirect uri for prod

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -6,7 +6,7 @@ import { Readable } from "stream";
 const oauth = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    "http://localhost:3001/oauth"
+    "http://uam-form.vercel.app/oauth"
 )
 
 oauth.setCredentials({
